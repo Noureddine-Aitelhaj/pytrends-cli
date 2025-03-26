3c8f9c862ccf5f7ddf31904282f6fd7186c1e72f
Noureddine-Aitelhaj <76914643+Noureddine-Aitelhaj@users.noreply.github.com>
2025-03-26 22:22:55 +0000

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Checking Chrome installation..."
-if ! command -v google-chrome &> /dev/null; then
-    echo "Chrome not found! Make sure it's installed in Dockerfile"
-    exit 1
-fi
-
 echo "Installing Python dependencies..."
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 echo "Installation completed successfully"
 pip list
