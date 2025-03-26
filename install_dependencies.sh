54f19a094a6ac83a8c5bd828c72d33123d2b2fda
Noureddine-Aitelhaj <76914643+Noureddine-Aitelhaj@users.noreply.github.com>
2025-03-26 20:58:34 +0000

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,6 +12,11 @@ pip install urllib3==1.25.11
 pip install pytrends
 pip install requests
 pip install googlesearch-python
+pip install facebook-page-scraper
+pip install selenium
+pip install webdriver-manager
+pip install python-dateutil
+pip install selenium-wire
 
 echo "Installation completed successfully"
 pip list
